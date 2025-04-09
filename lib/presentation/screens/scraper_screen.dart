7587f0aeaae81b690b279e9a12c03b04b3b2a41f
ps-vitor <the-jungleman@proton.me>
2025-04-09 17:04:37 -0300

    added scrapping screen

---
+++ lib/presentation/screens/scraper_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:docker_on_flutter/core/services/scraper_service.dart';
+
+class ScraperScreen extends StatefulWidget {
+  @override
+  _ScraperScreenState createState() => _ScraperScreenState();
+}
+
+class _ScraperScreenState extends State<ScraperScreen> {
+  final TextEditingController _controller = TextEditingController();
+  String result = "";
+
+  void fetchData() async {
+    final url = _controller.text.trim();
+    if (url.isEmpty) return;
+
+    final service = ScrapService("10.0.2.2");
+    final response = await service.scrapUrl(url);
+
+    setState(() {
+      result = response;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Web Scraper")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              labelText: 'URL to scrap',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          ElevatedButton(onPressed: fetchData, child: Text("Scrap")),
+          SizedBox(height: 20),
+          Expanded(
+              child: SingleChildScrollView(
+            child: Text(result),
+          ))
+        ]),
+      ),
+    );
+  }
+}
